a5e4abd423005cb1bb3161088c1dd16f1ac94a7a
Matheus Levy <levybessa2001@hotmail.com>
2022-05-04 19:27:38 -0300

    att reativoSimples Caminho Linear

--- Mapa.py
+++ Mapa.py
@@ -2,41 +2,47 @@ from utils import *
 
 class Mapa:
     
-    def __init__(self, m=20, n=20, objetos=10):
+    def __init__(self, m=20, n=20, objetos=[10, 10]):
         self.m = m
         self.n = n 
         self.objetos = objetos
-        self.objetosRestante = objetos
+        self.objetosRestante = sum(objetos)
         self.mapa = np.zeros((m,n), dtype= np.int8)    
     
     def generate(self, seedA):
         seed(seedA)
-        if(self.objetos > (self.n+1) * (self.m+1)):
+        if(sum(self.objetos) > (self.n+1) * (self.m+1)):
             print("Erro: Número de Objetos maior do que o Mapa!")
             return 0
         
-        Pontos = geradordeCoordenadasXY(self.objetos, self.n-1, self.m-1)
+        Pontos = geradordeCoordenadasXY(sum(self.objetos), self.n-1, self.m-1)
         print(Pontos)
+        
         input("Press Enter to continue...")
-        for ponto in Pontos:
-            random_m, random_n = ponto
+        for i in range(0, 10):
+            random_m, random_n = Pontos[i]
             self.mapa[random_m][random_n] = 1
-
+        
+        for i in range(10, 20):
+            random_m, random_n = Pontos[i]
+            self.mapa[random_m][random_n] = 2
                 
 
 
-    def toString(self, CaracterObjeto, CaracterVazio):
+    def toString(self, CaracterObjeto1, CaracterObjeto2, CaracterVazio):
         stringMapa = np.empty((self.m, self.n), dtype=object) #Criar a Matriz de Strings vazia
         for i in range(self.m):
             for j in range(self.n):
                 if self.mapa[i][j] == 1:                       #Onde tiver 1 vira o caractere de Objeto
-                    stringMapa[i][j] = CaracterObjeto   
+                    stringMapa[i][j] = CaracterObjeto1
+                elif self.mapa[i][j] == 2:
+                    stringMapa[i][j] = CaracterObjeto2   
                 else:
                     stringMapa[i][j] = CaracterVazio           #Onde tiver 0 vira caractere de Vazio
         return stringMapa
 
-    def renderizar(self, CaracterObjeto, CaracterVazio, X_Agente=0, Y_Agente=0 ):
-        render = self.toString(CaracterObjeto, CaracterVazio)
+    def renderizar(self, CaracterObjeto1, CaracterObjeto2, CaracterVazio, X_Agente=0, Y_Agente=0 ):
+        render = self.toString(CaracterObjeto1, CaracterObjeto2, CaracterVazio)
         render[Y_Agente][X_Agente] = 'A'
         for line in render:
             print ('  '.join(map(str, line)))
--- ReativoSimples.py
+++ ReativoSimples.py
@@ -17,14 +17,21 @@ class ReativoSimples:
         self.randomMov = randomMov
         self.x = 0
         self.y = 0
+        self.sentido = "direita"
         self.Ambiente = Mapa
+        self.pontuacao = 0
 
     def Clean(self, x, y):
+        if self.Ambiente.mapa[y][x] == 1:
+            pontos = 10
+        elif self.Ambiente.mapa[y][x] == 2:
+            pontos = 20
         self.Ambiente.mapa[y][x] = 0
         self.Ambiente.objetosRestante -= 1
         print("Clean")
+        return pontos
 
-    def Movimenta(self):
+    def MovimentaRandom(self):
         mv = random.choice(list(Movimento))
         print(mv)
         if mv == Movimento.Cima:
@@ -43,15 +50,39 @@ class ReativoSimples:
             if self.x + 1 < self.Ambiente.n:
                 self.x += 1
         
+    def MovimentaLinha(self):
+        if self.sentido == "direita":
+            if (self.x + 1 < self.Ambiente.n):
+                self.x +=1
+            else:
+                if(self.y + 1 < self.Ambiente.m):
+                    self.y +=1
+                    self.sentido = "esquerda"
+
+        elif self.sentido == "esquerda":
+            if(self.x -1 >= 0):
+                self.x -=1
+            else:
+               if(self.y + 1 < self.Ambiente.m):
+                    self.y +=1
+                    self.sentido = "direita"
 
 
     def RandomMovimentation(self):
         print("Localidade: " , self.Ambiente.mapa[self.y][self.x])
-        if self.Ambiente.mapa[self.y][self.x] == 1:
-            self.Clean(self.x,self.y)
-            self.Movimenta()
+        if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            self.pontuacao += self.Clean(self.x,self.y)
+            self.MovimentaRandom()
         else:
-            self.Movimenta()
+            self.MovimentaRandom()
+
+    def LinhaMovimentation(self):
+        print("Localidade: " , self.Ambiente.mapa[self.y][self.x])
+        if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            self.pontuacao += self.Clean(self.x,self.y)
+            self.MovimentaLinha()
+        else:
+            self.MovimentaLinha()
 
     def executar(self):
         while self.Ambiente.objetosRestante > 0:
@@ -60,15 +91,16 @@ class ReativoSimples:
             print("obj " , self.Ambiente.objetosRestante)
             print("SelfX = ", self.x)
             print("SelfY = ", self.y )
-            self.Ambiente.renderizar('@','*', X_Agente = self.x, Y_Agente = self.y)
-            self.RandomMovimentation()
+            self.Ambiente.renderizar('@','#','*', X_Agente = self.x, Y_Agente = self.y)
+            self.LinhaMovimentation()
             input("Press Enter to continue...")
     
             
             
-m1 = Mapa(m=5, n=5,objetos=10)
+m1 = Mapa(m=5, n=5)
 m1.generate(123)
 #m1.renderizar('@','*')
 
 r1 = ReativoSimples(m1)
 r1.executar()
+print("Pontuação Final: " , r1.pontuacao)
