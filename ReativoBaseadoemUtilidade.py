7d9446370c4b3276289f88ae53576859b69bbda8
Matheus Levy <levybessa2001@hotmail.com>
2022-05-05 13:08:32 -0300

    refatorado

--- ReativoBaseadoemUtilidade.py
+++ ReativoBaseadoemUtilidade.py
@@ -1,28 +1,20 @@
-from enum import Enum
-import math
 import os
-import sys
-
 from numpy import empty
 from Mapa import Mapa
+import numpy as np
+import time
 
-class Movimento(Enum):
-    Cima  = 1
-    Baixo = 2
-    Esquerda = 3
-    Direita = 4
-      
 class ReativoBaseadoemUtilizade:
-    
-    def __init__(self, Mapa, Objetivos, randomMov= True):
-        self.randomMov = randomMov
+    # O Agente Baseado em utilidade irá utilizar os objetivos também como lista de utilidade 
+    # A lista de objetivos deve ser passada de uma forma específica para isso
+    # Os pontos de maior utilidade devem ser colocados nas primeiras posições e os de menor utilidade nas ultimos
+    # Portanto, temos uma lista de objetivos em ordem decrescente de utilidade
+    def __init__(self, Mapa, Objetivos):
         self.x = 0
         self.y = 0
-        self.sentido = "direita"
         self.Ambiente = Mapa
         self.pontuacao = 0
         self.Objetivos = Objetivos
-        self.Rota = []
 
 
     def Clean(self, x, y):
@@ -32,57 +24,64 @@ class ReativoBaseadoemUtilizade:
             pontos = 20
         self.Ambiente.mapa[y][x] = 0
         self.Ambiente.objetosRestante -= 1
-        print("Clean")
         return pontos
 
+
+    def VerficaObjeto(self, ListadeObejetosPossiveis):
+        for objeto in ListadeObejetosPossiveis:
+            if self.Ambiente.mapa[self.y][self.x] == objeto:
+                return True
+        return False
+
+    def Render(self):
+        clear = lambda: os.system('cls')
+        clear()
+        self.Ambiente.renderizar('@','#','', X_Agente = self.x, Y_Agente = self.y)
+
+    # Função para Alcançar um Objetivo
+    # @Parametros:
+    # Objetivo: Um ponto(x,y)
+    # A função move o agente até a mesma coluna que o objetivo
+    # e depois move até a mesma linha que o objetivo
     def AlcancarObjetivo(self, Objetivo):
         if Objetivo == None:
             return None
         y, x = Objetivo
         while(self.x < x):
             self.x +=1
-            if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            if self.VerficaObjeto([1,2]):
                 self.pontuacao += self.Clean(self.x,self.y)
-            clear = lambda: os.system('cls')
-            clear()
-            self.Ambiente.renderizar('@','#','', X_Agente = self.x, Y_Agente = self.y)
+                self.Objetivos = list(filter((self.y, self.x).__ne__, self.Objetivos))
+            self.Render()
         while(self.x > x):
             self.x -=1
-            if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            if self.VerficaObjeto([1,2]):
                 self.pontuacao += self.Clean(self.x,self.y)
-            clear = lambda: os.system('cls')
-            clear()
-            self.Ambiente.renderizar('@','#','', X_Agente = self.x, Y_Agente = self.y)
-
+                self.Objetivos = list(filter((self.y, self.x).__ne__, self.Objetivos))
+            self.Render()
         while(self.y < y):
             self.y +=1
-            if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            if self.VerficaObjeto([1,2]):
                 self.pontuacao += self.Clean(self.x,self.y)
-            clear = lambda: os.system('cls')
-            clear()
-            self.Ambiente.renderizar('@','#','', X_Agente = self.x, Y_Agente = self.y)
-
+                self.Objetivos = list(filter((self.y, self.x).__ne__, self.Objetivos))
+            self.Render()
         while(self.y > y):
             self.y -=1
-            if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+            if self.VerficaObjeto([1,2]):
                 self.pontuacao += self.Clean(self.x,self.y)
-            clear = lambda: os.system('cls')
-            clear()
-            self.Ambiente.renderizar('@','#','', X_Agente = self.x, Y_Agente = self.y)
-            
-    def MovimentaObjetivo(self):
-        ProximoPonto = self.Objetivos.pop(0)
-        clear = lambda: os.system('cls')
-        clear()
-        print(ProximoPonto)
+                self.Objetivos = list(filter((self.y, self.x).__ne__, self.Objetivos))
+            self.Render()
 
-        self.AlcancarObjetivo(ProximoPonto)
+    #Função de setar o proximo objetivo e ir até ele
+    def MovimentaObjetivo(self):
+        print(self.Objetivos)
+        ProximoPonto = self.Objetivos.pop(0)    #Retira o primeiro elemento da lista de objetivos. Isto é, pois a lista de utilizades é a lista de objetivos.
+        print(ProximoPonto)
+        self.AlcancarObjetivo(ProximoPonto)     #Se movimenta até o proximo objetivo
 
     def ObjectiveMovimentation(self):
-        print("Localidade: " , self.Ambiente.mapa[self.y][self.x])
-        if self.Ambiente.mapa[self.y][self.x] == 1 or self.Ambiente.mapa[self.y][self.x] == 2:
+        if self.VerficaObjeto([1,2]):
             self.pontuacao += self.Clean(self.x,self.y)
-            self.MovimentaObjetivo()
         else:
             self.MovimentaObjetivo()
 
@@ -94,11 +93,20 @@ class ReativoBaseadoemUtilizade:
             self.ObjectiveMovimentation()
 
             
-            
-m1 = Mapa(m=20, n=20)
-ListadeUtilizades = m1.generate(123)
-#m1.renderizar('@','*')
+Tempos = []
+for _ in range(5):   
+    m1 = Mapa(m=20, n=20)
+    Pontos = m1.generate(123456)
+    inicio = time.process_time()
+    r1 = ReativoBaseadoemUtilizade(m1, Pontos)
+    r1.executar()
+    fim = time.process_time()
+    Tempos.append(fim-inicio)
 
-r1 = ReativoBaseadoemUtilizade(m1, ListadeUtilizades)
-r1.executar()
-print("Pontuação Final: " , r1.pontuacao)
+Tempos = np.array(Tempos)
+
+clear = lambda: os.system('cls')
+clear()
+print("Baseado em Objetivos")
+print("Tempos: ", Tempos)
+print("Media: ", np.mean(Tempos), "s(+-) ", np.std(Tempos))      
